9a4bf3b7ed83a0016c64102a0e3bace96f8e2db5
Mitchell Reeves <m326r722@ku.edu>
2022-08-07 10:36:03 -0400

    Initial commit

---
+++ Classes/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+FPS = 60
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+SKY_BLUE = (111, 179, 255)
+RED = (255,0,0)
+BLUE = (0,0,255)
+PINK = (216,89,225)
+GOLD = (255,215,0)
+GREEN = (0,255,0)
+HOT_PINK = (255,0,127)
+YELLOW = (255,255,0)
---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from constants import *
+import sys
+
+#FPS = 60
+
+win = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.init()
+screen_width = 1200
+screen_height = 700
+# screen = pygame.display.set_mode((screen_width,screen_height))
+
+
+
+def main():
+    running = True
+    clock = pygame.time.Clock()
+
+    while running:
+        clock.tick(FPS)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            running = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+main()
